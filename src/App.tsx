d8081408ee174bdc1e8ca6ba67b61925e7c75a3b
Tomas Lubertino <tomas.lubertino@gmail.com>
2021-11-03 20:06:25 -0300

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>HOLAAA</p>
+        <h1>Hello world</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
